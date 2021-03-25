bf3ed55d6a44f1198f28912f1ce1d284a9a1e42c
newaeonweb <fsilvasmonteiro@gmail.com>
2021-03-25 08:59:46 -0300

    add change theme

--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Title } from '@angular/platform-browser';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { Router } from '@angular/router';
 import { MatSidenav } from '@angular/material/sidenav';
 
@@ -10,13 +11,16 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class HeaderComponent implements OnInit {
   @Input() sidenav!: MatSidenav;
-  theme: string;
 
-  constructor(private titleService: Title) {}
+  constructor(@Inject(DOCUMENT) private document: any, private titleService: Title) {}
 
   ngOnInit() {}
 
   get title(): string {
     return this.titleService.getTitle();
+  }
+
+  changeTheme() {
+    this.document.body.classList.toggle('dark-theme');
   }
 }
