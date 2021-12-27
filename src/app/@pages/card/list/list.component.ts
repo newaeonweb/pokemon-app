61edbbbc114609ee078f1d2d66d95acddd84a2c9
newaeonweb <fsilvasmonteiro@gmail.com>
2021-12-27 14:28:56 -0300

    chore: remove extra code

--- src/app/@pages/card/list/list.component.ts
+++ src/app/@pages/card/list/list.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { BehaviorSubject, forkJoin, Observable, of } from 'rxjs';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { catchError, debounceTime, distinctUntilChanged, map, switchMap, tap } from 'rxjs/operators';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
 import { environment } from '@env/environment';
 import { FilterRequest, HttpApiResponse, PokemonService } from '../_services/pokemon.service';
@@ -17,16 +16,13 @@ const API_URL = environment.serverUrl;
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
-export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
+export class ListComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   characters$: Observable<any>;
-  characterDataSource: MatTableDataSource<any[]>;
-  characterDatabase = new HttpDatabase(this.httpClient);
   searchTerm$ = new BehaviorSubject<any>('');
   resultsEmpty$ = new BehaviorSubject<boolean>(false);
   resultsLength = 0;
 
-  filterFormGroup: FormGroup;
   searchField = new FormControl('');
   queryParams: Params;
 
@@ -35,13 +31,7 @@ export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
   subtypesIdValueFormControl = new FormControl('');
   setIdValueFormControl = new FormControl('');
 
-  searchLisText: string;
-
-  public filterValues = {
-    supertype: '',
-    types: '',
-    subtypes: '',
-  };
+  searchListText: string;
 
   typesList: Observable<string[]>;
   subtypesList: Observable<string[]>;
@@ -51,15 +41,12 @@ export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
   activatedRoute: ActivatedRoute;
 
   constructor(
-    private httpClient: HttpClient,
     public dialog: MatDialog,
     private fb: FormBuilder,
     private pokemonService: PokemonService,
     private route: ActivatedRoute,
     private router: Router
   ) {
-    this.filterFormGroup = this.fb.group({});
-
     this.characters$ = this.route.queryParams.pipe(
       switchMap((params) => {
         this.queryParams = {
@@ -89,7 +76,6 @@ export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.filterFormGroup = this.fb.group({});
     this.searchListening();
     this.loadFilters()
       .pipe(
@@ -103,12 +89,6 @@ export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
       .subscribe();
   }
 
-  ngOnDestroy() {
-    if (this.characterDataSource) {
-      this.characterDataSource.disconnect();
-    }
-  }
-
   updateUrlQueryParams() {
     this.router.navigate([], {
       relativeTo: this.activatedRoute,
@@ -118,10 +98,10 @@ export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   loadFilters(): Observable<{}> {
-    const typesList = this.pokemonService.getTypes();
-    const subtypesList = this.pokemonService.getSubtypes();
-    const supertypesList = this.pokemonService.getSupetypes();
-    const setList = this.pokemonService.getSets();
+    const typesList = this.pokemonService.getListFilters('types');
+    const subtypesList = this.pokemonService.getListFilters('subtypes');
+    const supertypesList = this.pokemonService.getListFilters('supertypes');
+    const setList = this.pokemonService.getListFilters('sets');
 
     return forkJoin([typesList, subtypesList, supertypesList, setList]);
   }
@@ -132,12 +112,15 @@ export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
         distinctUntilChanged(),
         switchMap(() => {
           this.characters$ = of(null);
-          return this.characterDatabase
+          this.queryParams.q =
+            this.supertypeValueFormControl.value !== '' ? `supertype:${this.supertypeValueFormControl.value}` : '';
+          this.updateUrlQueryParams();
+          return this.pokemonService
             .getAll({
               page: 1,
               pageSize: 20,
               orderBy: 'name',
-              q: this.supertypeValueFormControl.value !== '' ? `supertype:${this.supertypeValueFormControl.value}` : '',
+              q: this.queryParams.q,
             })
             .pipe(
               map((res: any) => {
@@ -177,21 +160,20 @@ export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
     //   .subscribe();
 
     this.typesValueFormControl.valueChanges.subscribe((types) => {
-      this.filterValues.subtypes = types;
-      this.characterDataSource.filter = JSON.stringify(this.filterValues);
-      console.log(this.characterDataSource.filter);
+      // this.filterValues.subtypes = types;
+      // this.characterDataSource.filter = JSON.stringify(this.filterValues);
+      // console.log(this.characterDataSource.filter);
     });
 
     this.subtypesIdValueFormControl.valueChanges.subscribe((subtypes) => {
-      this.filterValues.subtypes = subtypes;
-      this.characterDataSource.filter = JSON.stringify(this.filterValues);
+      // this.filterValues.subtypes = subtypes;
+      // this.characterDataSource.filter = JSON.stringify(this.filterValues);
     });
   }
 
   dispatchSearch(value: any) {
     this.queryParams.page = 1;
     this.queryParams.q = `name:${value}`;
-    // this.characters$ = of(null);
     this.updateUrlQueryParams();
     console.log('params on search', this.queryParams);
 
@@ -200,8 +182,6 @@ export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
 
   clearSearch() {
     this.searchField.setValue('');
-    // this.characters$ = of(null);
-    // this.searchTerm$.next('');
     this.queryParams.q = '';
     this.updateUrlQueryParams();
   }
@@ -212,72 +192,10 @@ export class ListComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   loadData() {
-    return this.characterDatabase.getCharactersList(this.queryParams).pipe(
+    return this.pokemonService.getCardsList(this.queryParams).pipe(
       map((result) => {
         this.resultsLength = result.totalCount;
-        this.characterDataSource = new MatTableDataSource(result.data as any[]);
-        // this.characterDataSource.paginator = this.paginator;
-        // this.characters$ = this.characterDataSource.connect();
-        // this.updateUrlQueryParams();
         return result.data;
-      })
-    );
-  }
-}
-
-export class HttpDatabase {
-  constructor(private _httpClient: HttpClient) {}
-
-  search(terms: Observable<any>) {
-    return terms.pipe(
-      debounceTime(400),
-      distinctUntilChanged(),
-      switchMap((term) => {
-        return this.getList(term).pipe(
-          catchError(() => {
-            return of({ data: null });
-          })
-        );
-      })
-    );
-  }
-
-  getList(params: any): Observable<HttpApiResponse> {
-    console.log(params);
-
-    const apiUrl = `${API_URL}/cards`;
-    return this._httpClient.get<HttpApiResponse>(apiUrl, {
-      params: new HttpParams()
-        .set('page', +params.page || 1)
-        .set('pageSize', params.pageSize || 20)
-        .set('orderBy', params.orderBy || 'name')
-        .set('q', params.q ? `name:${params.q}` : ''),
-    });
-  }
-
-  getCharactersList(params: Params): Observable<HttpApiResponse> {
-    const apiUrl = `${API_URL}/cards`;
-    return this._httpClient.get<HttpApiResponse>(apiUrl, {
-      params: new HttpParams()
-        .set('page', +params.page)
-        .set('pageSize', params.pageSize)
-        .set('orderBy', params.orderBy)
-        .set('q', params.q),
-    });
-  }
-
-  getAll(param?: any): Observable<any[]> {
-    let params = new HttpParams();
-
-    Object.keys(param).forEach((item) => {
-      params = params.set(item, param[item]);
-    });
-
-    const apiUrl = `${API_URL}/cards`;
-    return this._httpClient.get(apiUrl, { params }).pipe(
-      map((res: any) => res),
-      catchError((err: HttpErrorResponse) => {
-        return of(null);
       })
     );
   }
